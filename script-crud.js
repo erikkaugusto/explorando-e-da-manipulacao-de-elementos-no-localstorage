008b111fc8c96e1f7e36c58dfa6ada6f38420728
erikkaugusto <erik.ahlert@universo.univates.br>
2024-05-28 09:01:27 -0300

    editando tarefas

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,10 @@ const ulTarefas = document.querySelector('.app__section-task-list');
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []; // assim ele não vai iniciar vazio, o que foi passado anteriormente será carregado
 
+function atualizarTarefas() {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas)); // guarda a lista de tarefas dentro da localStorage, 'tarefas' é a chave de acesso e o JSON transforma em string a variável tarefas que será armazenada na localStorage
+}
+
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li');
     li.classList.add('app__section-task-list-item');
@@ -18,6 +22,17 @@ function criarElementoTarefa(tarefa) {
 
     const botao = document.createElement('button');
     botao.classList.add('app_button-edit');
+
+    botao.onclick = () => { // alterar nome da tarefa
+        debugger;
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?");
+        console.log('Nova descrição da tarefa: ', novaDescricao);
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao;
+            atualizarTarefas();
+        }
+    }
 
     const imagemBotao = document.createElement('img');
     imagemBotao.setAttribute('src', '/imagens/edit.png');
@@ -42,7 +57,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa);
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
-    localStorage.setItem('tarefas', JSON.stringify(tarefas)); // guarda a lista de tarefas dentro da localStorage, 'tarefas' é a chave de acesso e o JSON transforma em string a variável tarefas que será armazenada na localStorage
+    atualizarTarefas();
     textArea.value = '';
     formAdicionarTarefa.classList.add('hidden');
 })
